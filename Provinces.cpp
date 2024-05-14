4e40cc6159a59c9116b7a850246f6388b51e552f
code-walker-23 <chhabravinay549@gmail.com>
2024-05-14 20:10:07 +0530

    Initial commit

--- Provinces.cpp
+++ Provinces.cpp
@@ -1,1 +1,103 @@
-Provinces.cpp
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+// By making adjacency list
+
+class Solution {
+public:
+void makeAdjList(vector<vector<int>> isConnected, vector<int> adj[]){
+	int n = isConnected.size(); 	
+ 	for(int i = 0; i < n; i++){
+ 		for(int j = 0; j < n; j++){
+ 			if(isConnected[i][j] && i != j)
+ 				adj[i+1].push_back(j+1);
+ 		}
+ 	}
+
+}
+
+void bfs(int s, vector<int> adj[], vector<int> &visited) {
+    queue<int> q;
+    q.push(s);
+    visited[s] = 1;
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+
+        for (auto nbrs : adj[node]) {
+            if (!visited[nbrs]) {
+                q.push(nbrs);
+                visited[nbrs] = 1;
+            }
+        }
+    }
+}
+
+int findCircleNum(vector<vector<int>>& isConnected) {
+	int n = isConnected.size();
+	vector<int> adj[n+1];
+	makeAdjList(isConnected , adj);
+    vector<int>visited(n+1,0);
+	int noOfProvinces = 0;
+
+	for(int i = 1; i <= n; i++){
+		if(!visited[i]){
+			bfs(i,adj,visited);
+			noOfProvinces++;
+            cout << noOfProvinces << endl;
+		}
+	}
+	
+    return noOfProvinces;
+
+  }
+
+};
+
+// By using Adjancency Matrix
+
+class Solution {
+public:
+void bfs(int s, vector<vector<int>>& isConnected, vector<int> &visited) {
+    queue<int> q;
+    q.push(s);
+    visited[s] = 1;
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        
+        for(int i = 0; i < isConnected.size(); i++){
+            if(isConnected[node][i] && !visited[i]){
+                q.push(i);
+                visited[i] = 1;
+            }
+        }
+    }
+}
+
+int findCircleNum(vector<vector<int>>& isConnected) {
+	int n = isConnected.size();
+    
+    vector<int>visited(n,0);
+	int noOfProvinces = 0;
+    
+	for(int i = 0; i < n; i++){
+		if(!visited[i]){
+			bfs(i,isConnected,visited);
+			noOfProvinces++;
+		}
+	}
+    
+    return noOfProvinces;
+
+  }  
+
+};
+
+
+int main(){
+
+}
