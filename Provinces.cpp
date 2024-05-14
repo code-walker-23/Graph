053f47b38da2681ca545a0bba7a8bc0a1a7fbd83
code-walker-23 <chhabravinay549@gmail.com>
2024-05-14 22:13:51 +0530

    Updated

--- Provinces.cpp
+++ Provinces.cpp
@@ -2,7 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// By making adjacency list
+// By making adjacency list BFS
 
 class Solution {
 public:
@@ -56,7 +56,7 @@ int findCircleNum(vector<vector<int>>& isConnected) {
 
 };
 
-// By using Adjancency Matrix
+// By using Adjancency Matrix BFS
 
 class Solution {
 public:
@@ -98,6 +98,36 @@ int findCircleNum(vector<vector<int>>& isConnected) {
 };
 
 
-int main(){
+// By using Dfs
 
-}
+class Solution {
+public:
+ void dfs(int s ,vector<vector<int>> adj , vector<bool> &visited){
+        int n = adj.size();
+        visited[s] = true;
+        for(int i = 0; i < n; i++){
+            if(adj[s][i] && !visited[i]){
+                visited[i] = true;
+                dfs(i,adj,visited);
+            }
+        }
+        
+    }
+
+
+int findCircleNum(vector<vector<int>>& isConnected) {
+	int n = isConnected.size();
+    
+    vector<bool>visited(n,false);
+	int noOfProvinces = 0;
+    
+	for(int i = 0; i < n; i++){
+		if(!visited[i]){
+			dfs(i,isConnected,visited);
+			noOfProvinces++;
+		}
+	}
+    
+    return noOfProvinces;
+  }  
+};
